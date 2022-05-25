6665f02f0fb72ac02dd17ea554057ba20146d47c
Jakub Rozanski <jroaski@gmail.com>
2022-05-25 19:28:10 +0200

    Game Logic almost done

--- play_wordle.py
+++ play_wordle.py
@@ -1,14 +1,22 @@
 from wordle import Wordle
+
 def main():
     print("Hello Wordle")
     wordle = Wordle("APPLE")
+
     while wordle.can_attempt:
-        x = input("Type your guess:")
+        x = input("Type your guess: ")
+        if len(x) != wordle.WORD_LENGTH:
+            print(f"Word must be {wordle.WORD_LENGTH} letters long")
+            continue
         wordle.attempt(x)
+        result = wordle.guess(x)
+        print(*result, sep=" \n")
     if wordle.is_solved:
         print("DONE!")
     else:
         print("You've failed")
+
 
 if __name__ == '__main__':
 
